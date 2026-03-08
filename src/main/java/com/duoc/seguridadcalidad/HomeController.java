679008cd04c7451c78af462bb3dea17c55e7b907
CAL <cal@uc.cl>
2026-03-07 23:37:27 -0300

    agregar página de recetas

--- src/main/java/com/duoc/seguridadcalidad/HomeController.java
+++ src/main/java/com/duoc/seguridadcalidad/HomeController.java
@@ -11,13 +11,13 @@ public class HomeController {
     @GetMapping("/home")
     public String home(@RequestParam(name="name", required=false, defaultValue="Seguridad y Calidad en el Desarrollo") String name, Model model) { 
         model.addAttribute("name", name); 
-        return "Home"; 
+        return "recetas"; 
         } 
 
     @GetMapping("/")
     public String root(@RequestParam(name="name", required=false, defaultValue="Seguridad y Calidad en el Desarrollo") String name, Model model) { 
         model.addAttribute("name", name); 
-        return "Home"; 
+        return "recetas"; 
         } 
 }
 
